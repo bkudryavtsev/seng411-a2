a8b96d0519dc1de50861e44046c7287fa244ad35
Boris Kudryavtsev <boriskk.work@gmail.com>
2021-03-19 18:25:34 -0700

    Add trial generation and result output

--- index.js
+++ index.js
@@ -1,86 +1,150 @@
 import * as d3 from 'd3';
 
-// the canvas size
-let width = 400;
-let height = 400;
-
-//toggle bubble chart or text representation
-//const REPRESENTATION = 'text';
-const REPRESENTATION = 'bubble';
+const WIDTH = 400;
+const HEIGHT = 400;
 
 const MIN_VALUE = 0;
 const MAX_VALUE = 99;
-const NB_VALUES = 25;
+const NUM_REPETITIONS = 5;
+
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const userId = +urlParams.get('uid');
+
+const pad = 5; //padding for grid layout (text and bubble)
+
+const fontSize = 48; // arbitrary choice
 
 // keep a reference of the canvas
 const svg = d3.select('#app')
   .append('svg')
-  .attr('width', width)
-  .attr('height', height);
+  .attr('width', WIDTH)
+  .attr('height', HEIGHT);
 
-let values = d3.range(NB_VALUES).map(d => Math.floor(Math.random() * 100)) // the randomly generated set of values between 0 and 99
-console.log(values)
+function createTrials(conditions, numRep, bubbleFirst) {
+  const trials = [];
 
-let pad = 5 //padding for grid layout (text and bubble)
-let numCol, numRow; // number of columns, number of rows
-let bubble_min_radius, bubble_max_radius;
-let _w, _h;
-  
-let font_size;
-  
-if (NB_VALUES == 25){
-  numCol = 5;
-  numRow = 5;
-  _w = width/numCol
-  _h = height/numRow
+  conditions.forEach(condition => {
+    for (let i = 0; i < numRep; i++) {
+      trials.push([bubbleFirst ? 'bubble' : 'text', condition]);
+    }
+  });
 
-  bubble_min_radius = 1;// arbitrary, could be 0, or something else
-  bubble_max_radius = (_w/2 - pad*2);
-  
-  font_size = 48// arbitrary choice
+  conditions.forEach(condition => {
+    for (let i = 0; i < numRep; i++) {
+      trials.push([bubbleFirst ? 'text' : 'bubble', condition]);
+    }
+  });
+
+  return trials;
 }
 
-let sign = svg.selectAll('g') // create one group element to display each value, puts it at its position
-  .data(values)
-  .enter()
-  .append('g')
-  .attr('transform', (d, i) => 
-    `translate(${((i%numCol)*_w + (pad/2)*-1)},${((Math.floor(i/numRow))*_h + (pad/2)*-1)})`)
-  .on('click', d => {
-    console.log('clicked: ',d)
-  }).style('cursor','pointer')//make it a pointer on mouseover
+const trials = createTrials([3, 5, 9, 25], NUM_REPETITIONS, userId % 2 === 0);
+let currentTrial = 0;
 
-if (REPRESENTATION === 'bubble') {
-  //that's to create a perceptual scaling by mapping square root of value to radius, but other scaling functions could be used
-  let circleRadiusScale = d3.scaleLinear()
-      .domain([Math.sqrt(MIN_VALUE), Math.sqrt(MAX_VALUE)])
-      .range([bubble_min_radius, bubble_max_radius]);
+const results = [];
   
-  //create an 'invisible' circle of size half the max size of a bubble, simply to make it possible to click the smaller circles easily.
-  sign.append('circle')
-    .attr('cx', _w/2)
-    .attr('cy', _w/2)
-    .attr('r', bubble_max_radius/2)
-    .style('fill', 'white')
+function update(representation, n) {
+  const values = d3.range(n).map(d => MIN_VALUE + Math.floor(Math.random() * (MAX_VALUE - MIN_VALUE)));
 
-  // then, for each cell we appends a circle
-  sign.append('circle')
-    .attr('cx', _w/2)
-    .attr('cy', _w/2)
-    .attr('r', d => circleRadiusScale(Math.sqrt(d)))
-    .style('fill','black')
-} else if(REPRESENTATION === 'text'){
-  //create an 'invisible' circle of size half the max size of a bubble, simply to make it possible to click the smaller circles easily.
-  sign.append('circle')
-    .attr('cx', _w/2)
-    .attr('cy', _w/2)
-    .attr('r', bubble_max_radius/2)
-    .style('fill', 'white')
-  
-  sign.append('text')
-    .attr('x', _w/2)
-    .attr('y', _w/2)
-    .attr('text-anchor','middle')
-    .attr('font-size', font_size+'px')
-    .text(d => d)
+  let start;
+
+  svg.selectAll('g').remove();
+
+  let numCol, numRow;
+
+  if (n === 3 || n === 5) {
+    numCol = 5;
+    numRow = 5;
+  } else {
+    numCol = Math.round(Math.sqrt(n));
+    numRow = Math.round(Math.sqrt(n));
+  }
+
+  const _w = WIDTH/numCol;
+  const _h = HEIGHT/numRow;
+
+  const bubble_min_radius = 1;// arbitrary, could be 0, or something else
+  const bubble_max_radius = (_w/2 - pad*2);
+
+  const sign = svg.selectAll('g') // create one group element to display each value, puts it at its position
+    .data(values)
+    .enter()
+    .append('g')
+    .attr('transform', (d, i) => {
+      switch (n) {
+        case 3: {
+          const cols = [2, 1, 3];
+          const rows = [1, 3, 3];
+          return `translate(${(cols[i]*_w + (pad/2)*-1)},${(rows[i]*_h + (pad/2)*-1)})`;
+        }
+        case 5: {
+          const cols = [1, 3, 2, 1, 3];
+          const rows = [1, 1, 2, 3, 3];
+          return `translate(${(cols[i]*_w + (pad/2)*-1)},${(rows[i]*_h + (pad/2)*-1)})`;
+        }
+        case 9:
+          return `translate(${((i%3)*_w + (pad/2)*-1)},${((Math.floor(i/3))*_h + (pad/2)*-1)})`;
+      }
+
+      return `translate(${((i%numCol)*_w + (pad/2)*-1)},${((Math.floor(i/numRow))*_h + (pad/2)*-1)})`;
+    })
+    .on('click', (e, d) => {
+      if (start) {
+        results.push({
+          userId,
+          trial: currentTrial,
+          representation: representation,
+          duration: Date.now() - start,
+          error: Math.abs((d - d3.min(values)) / d3.max(values) - d3.min(values)),
+        });
+
+        const nextTrial = trials[++currentTrial];
+        if (nextTrial) {
+          update(...nextTrial);
+        } else {
+          alert('All trials complete!');
+          console.log(results);
+        }
+      }
+    }).style('cursor','pointer')//make it a pointer on mouseover
+
+  if (representation === 'bubble') {
+    //that's to create a perceptual scaling by mapping square root of value to radius, but other scaling functions could be used
+    let circleRadiusScale = d3.scaleLinear()
+        .domain([Math.sqrt(MIN_VALUE), Math.sqrt(MAX_VALUE)])
+        .range([bubble_min_radius, bubble_max_radius]);
+    
+    //create an 'invisible' circle of size half the max size of a bubble, simply to make it possible to click the smaller circles easily.
+    sign.append('circle')
+      .attr('cx', _w/2)
+      .attr('cy', _w/2)
+      .attr('r', bubble_max_radius/2)
+      .style('fill', 'white')
+
+    // then, for each cell we appends a circle
+    sign.append('circle')
+      .attr('cx', _w/2)
+      .attr('cy', _w/2)
+      .attr('r', d => circleRadiusScale(Math.sqrt(d)))
+      .style('fill','black')
+  } else if(representation === 'text') {
+    //create an 'invisible' circle of size half the max size of a bubble, simply to make it possible to click the smaller circles easily.
+    sign.append('circle')
+      .attr('cx', _w/2)
+      .attr('cy', _w/2)
+      .attr('r', bubble_max_radius/2)
+      .style('fill', 'white')
+    
+    sign.append('text')
+      .attr('x', _w/2)
+      .attr('y', _w/2)
+      .attr('text-anchor','middle')
+      .attr('font-size', fontSize+'px')
+      .text(d => d)
+  }
+
+  start = Date.now();
 }
+
+update(...trials[currentTrial]);
